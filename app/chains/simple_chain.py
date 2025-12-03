283945f5ff2c18978de4a8a2e708ddd6c0d0719e
TheSmartBeaver <totorx36bhu@gmail.com>
2025-12-03 14:10:58 +0100

    LLMChain is outdated

--- app/chains/simple_chain.py
+++ app/chains/simple_chain.py
@@ -3,7 +3,7 @@ from langchain_openai import ChatOpenAI
 from app.chains.llm.open_ai_gpt4o_mini_llm import OpenAiGPT4oMiniLlm
 from app.chains.prompt_templates.generation_prompt import FC_GENERATION_PROMPT
 from app.models.dto.user_entry.user_entry_dto import UserEntryDto
-from langchain.chains.llm import LLMChain
+# from langchain.chains.llm import LLMChain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.prompts.chat import (
     HumanMessagePromptTemplate,
@@ -25,12 +25,16 @@ def run_simple_chain(userEntryDto: UserEntryDto) -> str:
         input_variables=["job_desc", "estimate", "skills_compatibility", "previous_guess"], messages=messages
     )
 
-    chain = LLMChain(llm=openAiGPT4oMiniLlm, prompt=finalPrompt, verbose=True)
+    # LLMChain is downgraded
 
-    promptArgs = {
-        "topic": userEntryDto.context_entry.course
-    }
+    # chain = LLMChain(llm=openAiGPT4oMiniLlm, prompt=finalPrompt, verbose=True)
 
-    result = chain.run(promptArgs)
+    # promptArgs = {
+    #     "topic": userEntryDto.context_entry.course
+    # }
+
+    # result = chain.run(promptArgs)
+
+    result = "This is a placeholder response from run_simple_chain."
 
     return result

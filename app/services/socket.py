814d9f4bf35493478823e921db0d9ab19593f351
TheSmartBeaver <totorx36bhu@gmail.com>
2025-12-03 15:30:36 +0100

    modif socket.io to try to connect

--- app/services/socket.py
+++ app/services/socket.py
@@ -8,3 +8,19 @@ sio = socketio.AsyncServer(cors_allowed_origins="*")
 
 def socket_notify(event: str, data: dict):
     sio.emit(event, data)
+
+@sio.event
+def connect(sid, environ):
+    print("✔️ Client connecté :", sid)
+
+@sio.event
+def disconnect(sid):
+    print("❌ Client déconnecté :", sid)
+
+@sio.on("generate_flashcard")
+def generate_flashcard(sid, data):
+    print("📥 Reçu :", data)
+
+    # Exemple : renvoyer une flashcard
+    sio.emit("flashcard_generated", {"question": "Test", "answer": "OK"})
+    print("📤 Flashcard envoyée")

eeed42fd6328757f7637ef425fec6c87d141ef41
TheSmartBeaver <totorx36bhu@gmail.com>
2025-12-03 21:41:35 +0100

    try to fix

--- app/services/lifespan.py
+++ app/services/lifespan.py
@@ -7,13 +7,15 @@ from app.services.socket import redis_listener
 @asynccontextmanager
 async def customlifespan(app: FastAPI):
     # 🚀 Start Redis listener
-    task = asyncio.create_task(redis_listener())
+    # task = asyncio.create_task(redis_listener())
+    print("✔️ Redis listener starting")
+    await redis_listener()
     print("✔️ Redis listener started")
     yield  # Application is running here
 
     # 🔻 Shutdown
-    task.cancel()
-    try:
-        await task
-    except asyncio.CancelledError:
-        pass
+    # task.cancel()
+    # try:
+    #     await task
+    # except asyncio.CancelledError:
+    #     pass

47abbdbffaf4aefd870577411a583ff50626f1fb
TheSmartBeaver <totorx36bhu@gmail.com>
2025-12-03 22:09:12 +0100

    try to fix 5

--- app/services/lifespan.py
+++ app/services/lifespan.py
@@ -8,9 +8,8 @@ from app.services.socket import redis_listener
 async def customlifespan(app: FastAPI):
     # 🚀 Start Redis listener
     print("✔️ Redis listener starting")
-    await redis_listener()
-    task = asyncio.create_task(redis_listener())
     # await redis_listener()
+    task = asyncio.create_task(redis_listener())
     print("✔️ Redis listener started")
     yield  # Application is running here
 

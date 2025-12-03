f2f693540a0c4f9d5839c17241106fe222c1932f
TheSmartBeaver <totorx36bhu@gmail.com>
2025-12-03 19:59:20 +0100

    await does not work with celery

--- app/workers/tasks.py
+++ app/workers/tasks.py
@@ -1,15 +1,16 @@
+import asyncio
 from .celery_app import celery
 from app.chains.generator import generate_flashcard
 from app.services.socket import socket_notify
 
 @celery.task(name="generate.flashcard")
-async def generate_flashcard_task(task_id: str, instructions: dict):
+def generate_flashcard_task(task_id: str, instructions: dict):
     print("📥 Starting generation")
     flashcard = generate_flashcard(instructions)
     print("📥 Generation ended")
-    await socket_notify(
+    asyncio.run(socket_notify(
         event="flashcard_generated",
         data={"task_id": task_id, "flashcard": flashcard}
-    )
+    ))
     print("📥 Celery task ended")
     return flashcard

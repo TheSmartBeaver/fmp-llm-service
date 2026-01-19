4976d56f6c151ec54e0ef676dffe849f538fa531
TheSmartBeaver <totorx36bhu@gmail.com>
2026-01-19 17:55:06 +0100

    additional instructions can be null

--- app/models/dto/user_entry/context_entry_dto.py
+++ app/models/dto/user_entry/context_entry_dto.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class ContextEntryDto(BaseModel):
     course: str # Do we know in which course the flashcards will be used
     topic_path: str # Do we know in which topic the flashcards will be used
-    additional_instructions: str = "" # Any specific instructions for flashcard generation
+    additional_instructions: Optional[str] = "" # Any specific instructions for flashcard generation
     fc_to_modify: str # Do we know if we have to modify existing flashcard
